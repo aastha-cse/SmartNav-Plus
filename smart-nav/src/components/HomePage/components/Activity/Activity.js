a4cff09e03f8c631cd4d20174fe5478189dc345c
aastha-cse <guptaaastha824@gmail.com>
2024-05-13 12:08:32 +0530

    added activity section in home page

--- smart-nav/src/components/HomePage/components/Activity/Activity.js
+++ smart-nav/src/components/HomePage/components/Activity/Activity.js
@@ -1,92 +1,92 @@
-// import React, { useRef, useEffect, useState } from "react";
-// import "./Activity.css";
-// import Map from "./images/map.png";
-// import Weather from "./images/weather.png";
-// import Fuel from "./images/fuel.png";
+import React, { useRef, useEffect, useState } from "react";
+import "./Activity.css";
+import Sight from "./images/sight.png";
+import Adventure from "./images/adventure.png";
+import Petrol from "./images/petrol.png";
 
-// const SlideInSection = ({ image, heading, text, direction }) => {
-//   const sectionRef = useRef(null);
-//   const [isVisible, setIsVisible] = useState(false);
+const SlideInSection = ({ image, heading, text, direction }) => {
+  const sectionRef = useRef(null);
+  const [isVisible, setIsVisible] = useState(false);
 
-//   useEffect(() => {
-//     let currentRef = sectionRef.current;
+  useEffect(() => {
+    let currentRef = sectionRef.current;
 
-//     const observer = new IntersectionObserver(
-//       ([entry]) => {
-//         if (entry.isIntersecting) {
-//           setIsVisible(true);
-//           observer.unobserve(currentRef);
-//         }
-//       },
-//       {
-//         rootMargin: "0px",
-//         threshold: 0.5,
-//       }
-//     );
+    const observer = new IntersectionObserver(
+      ([entry]) => {
+        if (entry.isIntersecting) {
+          setIsVisible(true);
+          observer.unobserve(currentRef);
+        }
+      },
+      {
+        rootMargin: "0px",
+        threshold: 0.5,
+      }
+    );
 
-//     if (currentRef) {
-//       observer.observe(currentRef);
-//     }
+    if (currentRef) {
+      observer.observe(currentRef);
+    }
 
-//     return () => {
-//       if (currentRef) {
-//         observer.unobserve(currentRef);
-//       }
-//     };
-//   }, []);
+    return () => {
+      if (currentRef) {
+        observer.unobserve(currentRef);
+      }
+    };
+  }, []);
 
-//   const sectionClass = `section-${direction} ${
-//     isVisible ? "section-visible" : ""
-//   }`;
+  const sectionClass = `section-${direction} ${
+    isVisible ? "section-visible" : ""
+  }`;
 
-//   return (
-//     <div className={`section ${sectionClass}`} ref={sectionRef}>
-//       <div className="image-container">
-//         <img src={image} alt="Section" />
-//       </div>
-//       <div className="text-container">
-//         <h3>{heading}</h3>
-//         <p>{text}</p>
-//       </div>
-//     </div>
-//   );
-// };
+  return (
+    <div className={`section ${sectionClass}`} ref={sectionRef}>
+      <div className="image-container">
+        <img src={image} alt="Section" />
+      </div>
+      <div className="text-container">
+        <h3>{heading}</h3>
+        <p>{text}</p>
+      </div>
+    </div>
+  );
+};
 
-// const Activity = () => {
-//   const sections = [
-//     {
-//       image: Map,
-//       heading: "Discover the Shortest Route",
-//       text: "This feature empowers users to find the most efficient path between two or more locations, optimizing travel time and minimizing distances. By leveraging advanced mapping algorithms, our application ensures seamless navigation, allowing users to reach their destinations swiftly and with ease.",
-//       direction: "right",
-//     },
-//     {
-//       image: Weather,
-//       heading: "Real Time Weather Forecast",
-//       text: "Stay informed and plan your journey more effectively with our real-time weather forecast feature integrated into our map application. Receive up-to-the-minute weather updates, including temperature, humidity, wind speed, and more, directly on the map interface.",
-//       direction: "left",
-//     },
-//     {
-//       image: Fuel,
-//       heading: "Estimated Fuel Cost",
-//       text: "Users can make informed decisions about their travel routes and budgeting by receiving an approximation of the fuel expenses associated with their journey. By factoring in variables such as distance, vehicle fuel efficiency, and current fuel prices, this feature helps users plan their trips effectively.",
-//       direction: "right",
-//     },
-//   ];
+const Activity = () => {
+  const sections = [
+    {
+      image: Sight,
+      heading: "Sights to See",
+      text: "Provides information and directions to popular tourist destinations, landmarks, historical sites, museums, art galleries, amusement parks, and other points of interest.",
+      direction: "right",
+    },
+    {
+      image: Adventure,
+      heading: "Explore Outdoor Adventures",
+      text: "Provides guidance for outdoor recreational activities such as hiking trails, biking routes, camping grounds, beaches, parks, and nature reserves.",
+      direction: "left",
+    },
+    {
+      image: Petrol,
+      heading: "Discover Nearby Fuel Stations",
+      text: "Locate nearby fuel stations effortlessly with our navigation app. Whether you're on a road trip or in need of a quick refill, find the nearest petrol pumps with ease, ensuring a smooth journey ahead.",
+      direction: "right",
+    },
+  ];
 
-//   return (
-//     <div className="container">
-//       {sections.map((section, index) => (
-//         <SlideInSection
-//           key={index}
-//           image={section.image}
-//           heading={section.heading}
-//           text={section.text}
-//           direction={section.direction}
-//         />
-//       ))}
-//     </div>
-//   );
-// };
+  return (
+    <div className="container">
+      {sections.map((section, index) => (
+        <SlideInSection
+          key={index}
+          image={section.image}
+          heading={section.heading}
+          text={section.text}
+          direction={section.direction}
+        />
+      ))}
+    </div>
+  );
+};
 
-// export default Activity;
+export default Activity;
